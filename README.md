665d7078848a9880f40bfb55316bc022768a264f
christopher.hogg <christopher.hogg@blik.io>
2019-06-04 19:26:11 +0100

    feat: create one file

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 - create a gatsby page from the command line
 - set the settings fromthe command line
 - deploy from the command line
+
+## Tested with yarn
+1. Install 
+`yarn add gatsby gatsby-theme-modern-portfolio commander-pages -D`
+2. Run the command
+`yarn create-site1`
+## Thanks to
+https://docs.npmjs.com/creating-and-publishing-scoped-public-packages
--- bin/commit.js
+++ bin/commit.js
@@ -12,11 +12,18 @@ program
   .parse(process.argv);
 
 
-  var dir = './tmp';
+  var dir = './site/';
+  var templateDir = './templates/';
 
+  var text = fs.readFileSync(templateDir + 'gatsby-config.js','utf8')
   if (!fs.existsSync(dir, { recursive: true })){
     fs.mkdirSync(dir, { recursive: true });
       console.log('created')
   }  
 
-console.log(program.args);
+  fs.writeFile(dir + "gatsby-config.js", text, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
